8afadcf710cca22929bca7cf610d7da29e5774fd
kenzo03 <kevinwijaya480@gmail.com>
2022-03-17 22:28:06 +0700

    add card_article.dart

---
+++ lib/widgets/card_article.dart
@@ -0,0 +1,26 @@
+import 'package:codelab2_news_app/ui/article_detail_page.dart';
+import 'package:flutter/material.dart';
+import '../data/model/article.dart';
+import '../common/styles.dart';
+
+class CardArticle extends StatelessWidget {
+  final Article article;
+  const CardArticle({Key? key, required this.article}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: primaryColor,
+        child: ListTile(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          leading: Hero(
+              tag: article.urlToImage!,
+              child: Image.network(article.urlToImage!, width: 100)),
+          title: Text(article.title),
+          subtitle: Text(article.author!),
+          onTap: () => Navigator.pushNamed(context, ArticleDetailPage.routeName,
+              arguments: article),
+        ));
+  }
+}
