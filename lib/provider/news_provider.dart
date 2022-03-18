bc9c946fe67dea06e978548e67cc6ca025c64f4d
kenzo03 <kevinwijaya480@gmail.com>
2022-03-18 22:59:12 +0700

    add provider and add news_provider.dart

---
+++ lib/provider/news_provider.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/foundation.dart';
+
+import '../data/model/article.dart';
+import '../data/api/api_service.dart';
+
+enum ResultState { Loading, NoData, HasData, Error }
+
+class NewsProvider extends ChangeNotifier {
+  final ApiService apiService;
+
+  NewsProvider({required this.apiService}) {
+    _fetchAllArticle();
+  }
+
+  late ArticlesResult _articleResult;
+  late ResultState _state;
+  String _message = '';
+
+  String get message => _message;
+
+  ArticlesResult get result => _articleResult;
+
+  ResultState get state => _state;
+
+  Future<dynamic> _fetchAllArticle() async {
+    try {
+      _state = ResultState.Loading;
+      notifyListeners();
+      final article = await apiService.topHeadlines();
+      if (article.articles.isEmpty) {
+        _state = ResultState.NoData;
+        notifyListeners();
+        return _message = 'Empty Data';
+      } else {
+        _state = ResultState.HasData;
+        notifyListeners();
+        return _articleResult = article;
+      }
+    } catch (e) {
+      _state = ResultState.Error;
+      notifyListeners();
+      return _message = 'Error --> $e';
+    }
+  }
+}
